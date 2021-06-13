e38a5ea67d7dc785c0245af75464903ee9e8f79e
Ian Miller <imiller142@gmail.com>
2021-06-13 13:13:53 -0500

    Second commit working on implementing playing

--- game.py
+++ game.py
@@ -37,6 +37,51 @@ class Deck:
         #Note we remove one card from the list of all_cards
         return self.all_cards.pop()
 
-mydeck = Deck()
+class Player:
+         
+    def __init__(self, name):
+        self.name = name
+        #A new player has no cards
+        self.all_cards = []
 
-len(mydeck.all_cards)
+    def remove_one(self):
+        # Note we remove one card from the list of all_cards
+        # We state 0 to remove from the "top" of the deck
+        # We'll imagine index -1 as the bottom of the deck
+        return self.all_cards.pop(0)
+
+    def add_cards(self, new_cards):
+        if type(new_cards) == type([]):
+            self.all_cards.extend(new_cards)
+        else:
+                self.all_cards.append(new_cards)
+
+        def __str__(self):
+            return f'Player {self.name} has {len{self.all_cards)} cards.'
+
+player_one = Player("One")
+
+player_two = Player("Two")
+
+new_deck = Deck()
+new_deck.shuffle()
+
+for x in range(26):
+    player_one.add_cards(new_deck.deal_one())
+    player_two.add_cards(new_deck.deal_one())
+
+import pdb
+
+game_on = True
+
+round_num = 0
+while game_on:
+
+    round_num += 1
+    print(f"Round {round_num}")
+
+    #Check to see if a player is out of cards:
+    if len(player_one.all_cards) == 0:
+        print("Player One is out of cards! Game Over")
+        print("Player Two Wins!")
+        game_on = False
